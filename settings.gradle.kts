93df407d63201e05717cacd6764f5fed4a288ac3
Grey <239888622+Dialupghost@users.noreply.github.com>
2026-08-08 20:32:37 -0500

    Extract source from RevengeLauncher-github-upload.zip and expand full Android project

---
+++ settings.gradle.kts
@@ -0,0 +1,19 @@
+// /data/user/0/com.codex.mobile/files/home/codex/RevengeLauncher/settings.gradle.kts
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "RevengeLauncher"
+include(":app")
